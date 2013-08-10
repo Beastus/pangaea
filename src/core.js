195e8e259defcb5b3ab908a5edd2e0455f845744
Porkchop Express <primitive.brute.beast@gmail.com>
2013-08-09 21:23:56 -0400

    minor refactoring--still planning

--- src/core.js
+++ src/core.js
@@ -9,7 +9,8 @@ var pan = pan || {};
 	"use strict";
 
 	/**
-	 * @namespace FLAGS
+	 * @namespace pan.FLAGS
+	 * @class {Object}
 	 * @desc Contains debug-related flags
 	 */
 	pan.FLAGS = {
@@ -19,10 +20,11 @@ var pan = pan || {};
 	};
 
 	/**
-	 * @object pan.core
+	 * @class {Object}
 	 * @desc 
 	 */
 	pan.core = {
+		queue: [],
 		start: function () {
 			pan.core.last = Date.now();
 		},
@@ -39,15 +41,19 @@ var pan = pan || {};
 		},
 		update: function () {
 			// update state
+			var i;
+			for (i = 0; i < this.queue.length; i++) {
+				this.queue[i].update();
+			}
 		},
 		render: function () {
 			// draw to canvas
+			var i;
+			for (i = 0; i < this.queue.length; i++) {
+				this.queue[i].render();
+			}
 		}
 	};
-
-	//
-	// augmentation
-	//
 
 	// Setup convenient prototypal inheritance function
 	// Example: newObject = Object.create(oldObject);
@@ -59,47 +65,11 @@ var pan = pan || {};
 			return new F();
 		};
 	}
-
-	/**
-	 * Returns a number whose value is limited to the given range.
-	 *
-	 * Example: limit the output of this computation to between 0 and 255
-	 * (x * 255).clamp(0, 255)
-	 *
-	 * @param {Number} min The lower boundary of the output range
-	 * @param {Number} max The upper boundary of the output range
-	 * @returns A number in the range [min, max]
-	 * @type Number
-	 */
-	Number.prototype.clamp = function (min, max) {
-		return Math.min(Math.max(this, min), max);
-	};
-
-	/**
-	 * Draws an ellipse to the canvas 2d rendering context.
-	 *
-	 * @param {number} centerX The x coordinate of the center point of the ellipse.
-	 * @param {number} centerY The y coordinate of the center point of the ellipse.
-	 * @param {number} width The semi-major axis of the ellipse.
-	 * @param {number} height The semi-minor axis of the ellipse.
-	 */
-	CanvasRenderingContext2D.prototype.ellipse = function (centerX, centerY, width, height) {
-
-		this.moveTo(centerX, centerY - (height / 2)); // A1
-
-		this.bezierCurveTo(centerX + (width / 2), centerY - (height / 2), // C1
-			centerX + (width / 2), centerY + (height / 2), // C2
-			centerX, centerY + (height / 2)); // A2
-
-		this.bezierCurveTo(centerX - (width / 2), centerY + (height / 2), // C3
-			centerX - (width / 2), centerY - (height / 2), // C4
-			centerX, centerY - (height / 2)); // A1
-	};
 }());
 
 // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-// requestAnimationFrame polyfill by Erik Möller. Fixes from Paul Irish and Tino Zijdel.
+// requestAnimationFrame polyfill by Erik Moller. Fixes from Paul Irish and Tino Zijdel.
 // MIT license
 (function () {
 	"use strict";
@@ -107,7 +77,8 @@ var pan = pan || {};
     var lastTime = 0, vendors = ['ms', 'moz', 'webkit', 'o'], x, currTime, timeToCall, id;
     for (x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
         window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
+			window[vendors[x] + 'CancelRequestAnimationFrame'];
     }
     if (!window.requestAnimationFrame) {
         window.requestAnimationFrame = function (callback, element) {
---
+++ src/util.js
@@ -0,0 +1,70 @@
+/**
+ * @namespace pan.util
+ * 
+ */
+(function () {
+	"use strict";
+
+	pan.util = pan.util || {};
+
+	/**
+     * @constructor 
+     * @memberof pan.util
+     */
+	pan.util.DeltaTimer = function () {
+
+		var startTime, frames = 0, frameRate = 0, last = 0, target = 0, overflow = 0, resetThreshold = 80, resetCount = 0;
+		// return the object that we want to expose
+		return {
+			start: function (fps) {
+				last = new Date().getTime();
+				target = 1000 / fps;
+				startTime = new Date().getTime();
+			},
+			ready: function () {
+				var diff = (new Date().getTime() - last) + overflow;
+				if (diff >= target) {
+					overflow = diff - target;
+					if (overflow > resetThreshold) {
+						// reset on assumption that tab was suspended
+						overflow = 0;
+						frames = 0;
+						startTime = new Date().getTime();
+						resetCount += 1;
+						// this pushes up the threshold to accommodate possible super slow performance
+						if (resetCount > 5) {
+							resetThreshold += 20;
+							resetCount = 0;
+						}
+					}
+					last = new Date().getTime();
+					frames++;
+					if ((frames % 10) === 0) {
+						// update FPS calculation
+						frameRate = frames / ((new Date().getTime() - startTime) / 1000);
+					}
+					return true;
+				}
+				return false;
+			},
+			getFrameRate: function () {
+				return Math.round(frameRate * 1000, 10) / 1000;
+			}
+		};
+	};
+
+	/**
+	 * Returns a number whose value is limited to the given range.
+	 *
+	 * Example: limit the output of this computation to between 0 and 255
+	 * (x * 255).clamp(0, 255)
+	 *
+	 * @param {Number} min The lower boundary of the output range
+	 * @param {Number} max The upper boundary of the output range
+	 * @returns A number in the range [min, max]
+	 * @type Number
+	 */
+	Number.prototype.clamp = function (min, max) {
+		return Math.min(Math.max(this, min), max);
+	};
+}());
